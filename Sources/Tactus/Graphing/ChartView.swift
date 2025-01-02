7cfc896782366b7c6f1f92fa3af1b0df7b9d9fa0
Ben Palmer <benj.c.palmer@gmail.com>
2025-01-02 10:33:27 -0500

    playing with swift charts / jittering for eventual spline interpolation

--- Sources/Tactus/Graphing/ChartView.swift
+++ Sources/Tactus/Graphing/ChartView.swift
@@ -1,48 +1,95 @@
 import SwiftUI
 import Charts
+import GameplayKit
 
+typealias Function = @Sendable (Double) -> Double
 
 struct ContentView: View {
     @State var slider: Double = 1
     @ObservedObject var presenter = ChartPresenter()
     
     var body: some View {
-        ScrollView {
-            VStack {
-                chart
-                    .padding()
-                    .padding(.top, 100)
-            }
+        VStack {
+            Spacer()
+            chart
+                .padding()
+            Spacer()
         }
     }
     
     var chart: some View {
-        Chart {
-            LinePlot(x: "x", y: "y") { [f = presenter.function] x in
-                f(x)
+        Group {
+            let data = presenter.jitteredPoints(
+                with: presenter.function,
+                from: 0,
+                to: 10,
+                steps: 50
+            )
+            Chart(data) {
+                PointMark(x: .value("x", $0.x), y: .value("y", $0.y))
+                    .foregroundStyle(.red)
+            }
+            Chart {
+                Plot {
+                    LinePlot(x: "x", y: "y") { [f = presenter.function] x in
+                        f(x)
+                    }
+                    .foregroundStyle(.blue)
+                    .lineStyle(.init(lineWidth: 3, lineCap: .round, lineJoin: .round))
+                }
             }
         }
-        .chartXScale(domain: -0...4)
-        .frame(width: 1200, height: 620)
+        .padding(32)
+        .chartXScale(domain: 0...10)
+        .frame(height: 400)
+        .background(
+            Color(UIColor.secondarySystemBackground)
+                .cornerRadius(8)
+        )
+        
     }
 }
 
+struct PlotData: Identifiable {
+    let id: Int
+    let x: Double
+    let y: Double
+}
+
 class ChartPresenter: ObservableObject {
-    typealias Function = @Sendable (Double) -> Double
-    
-    
-    
     @Published var function: Function = { x in
-        sin(-3 * pow(x, 2))
+        cos(x/2) * sin(x*2)
     }
+    private let gaussian = GKGaussianDistribution(
+        randomSource: GKRandomSource(),
+        mean: 0,
+        deviation: 5
+    )
     
-    func calculateRiemannSumDataFor(
-        _ function: @escaping Function,
+    func jitteredPoints(
+        with function: @escaping Function,
         from start: Double,
         to end: Double,
-        divisionWidth: Int
-    )  {
+        steps: Int
+    )  -> [PlotData] {
         
+        // get the length of the x axis
+        let xAxisLength = sqrt((end - start)**2)
+        
+        // get bar width (step size)
+        let stepSize = xAxisLength / Double(steps)
+        
+        // compute array of x values for each step across range
+        let xValues: [Double] = Array(stride(from: start, through: end, by: stepSize))
+        
+        // generate plotData via f(x)
+        return xValues.enumerated().map { i, x in
+            let xJitter = Double(gaussian.nextUniform()) * 0.2
+            let yJitter = Double(gaussian.nextUniform()) * 0.2
+            let x = xValues[i]
+            let y = function(x)
+            return PlotData(id: i, x: x + xJitter, y: y + yJitter)
+        }
     }
 }
 
---
+++ Sources/Tactus/Utilities/ExponentOperator.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+precedencegroup ExponentiationPrecedence {
+  associativity: right
+  higherThan: MultiplicationPrecedence
+}
+
+infix operator ** : ExponentiationPrecedence
+
+func ** (_ base: Double, _ exp: Double) -> Double {
+  return pow(base, exp)
+}
+
+func ** (_ base: Float, _ exp: Float) -> Float {
+  return pow(base, exp)
+}
